1dae1640f012bf931fdb974958eff9ece95cdc75
Jackson Atkins <Jacksonatkins@amdg.rockhursths.edu>
2018-11-30 13:59:43 -0600

    add users table

--- DB/db.sql
+++ DB/db.sql
@@ -7,6 +7,17 @@ CREATE TABLE IF NOT EXISTS messages (
     PRIMARY KEY (message_id)
 );
 
+CREATE TABLE IF NOT EXISTS users (
+    user_id     VARCHAR(26)     NOT NULL,
+    fullName    VARCHAR(256)    NOT NULL,
+    givenName   VARCHAR(256)    NOT NULL,
+    familyName  VARCHAR(256)    NOT NULL,
+    email       VARCHAR(256)    NOT NULL,
+    imageUrl    VARCHAR(256)    NOT NULL,
+    PRIMARY KEY (user_id)    
+);
+
+
 
 INSERT INTO messages(timestamp, sender, recipient, message)
 VALUES(NOW(), 'Me', 'You', 'Hi!');
